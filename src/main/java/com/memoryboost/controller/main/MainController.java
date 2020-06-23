533648b74f13b16cc5f3d83b7422917a2fc1cfc0
jjh <pursue503@naver.com>
2020-06-23 19:45:50 +0900

    Add mkdir test 4

--- src/main/java/com/memoryboost/controller/main/MainController.java
+++ src/main/java/com/memoryboost/controller/main/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileWriter;
 
 @Slf4j
 @Controller
@@ -26,7 +27,7 @@ public class MainController {
     @GetMapping("/test")
     public String test(){
 
-        String path = "/home/ec-user/upload/name";
+        String path = "/home/ec2-user/upload/name";
 
         File folder = new File(path);
 
@@ -34,21 +35,6 @@ public class MainController {
             try{
                 folder.mkdirs();
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return "redirect:/";
-
-    }
-
-    @GetMapping("/test2")
-    public String test2(){
-        String path = "\\home\\ec-user\\upload\\name";
-        File folder = new File(path);
-        if(!folder.exists()) {
-            try{
-                folder.mkdirs();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -58,37 +44,22 @@ public class MainController {
 
     @GetMapping("/test3")
     public String test3(){
-        String path = "/upload2";
 
-        File folder = new File(path);
+        String txt = "안녕하세요";
+        String filePath = "/home/ec2-user/upload/tetet.txt";
 
-        if(!folder.exists()) {
-            try{
-                folder.mkdirs();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        try{
+            File file = new File(filePath);
+
+            FileWriter fw = new FileWriter(file,true);
+            fw.write(txt);
+            fw.flush();
+            fw.close();
+        } catch (Exception e ) {
+            e.printStackTrace();
         }
 
         return "redirect:/";
-
-    }
-    @GetMapping("/test4")
-    public String test4(){
-        String path = "/upload2";
-
-        File folder = new File(path);
-
-        if(!folder.exists()) {
-            try{
-                folder.mkdirs();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        return "redirect:/";
-
     }
 
 }
