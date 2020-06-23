66745a3412b26e1009ec84161904d95861ac7c2f
jjh <pursue503@naver.com>
2020-06-23 18:55:07 +0900

    Add mkdir test

--- src/main/java/com/memoryboost/controller/main/MainController.java
+++ src/main/java/com/memoryboost/controller/main/MainController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 
 @Slf4j
 @Controller
@@ -14,11 +16,30 @@ public class MainController {
     @GetMapping("/")
     public String mainPage(HttpServletRequest request, Model model){
 
-        String path = request.getSession().getServletContext().getRealPath("\\") + "/test";
+        ServletContext path = request.getSession().getServletContext();
 
         model.addAttribute("path",path);
 
         return "main";
+    }
+
+    @GetMapping("/test")
+    public void test(){
+
+        String path = "/home/ec-user/upload/name";
+
+        File folder = new File(path);
+
+        if(!folder.exists()) {
+            try{
+                folder.mkdir();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
 }
