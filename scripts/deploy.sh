b85c8fb8c10b0d516ffdba7f869028c45101f0bf
jjh <pursue503@naver.com>
2020-07-06 13:05:01 +0900

    Re build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-mail.yml \
+    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-mail.yml,/home/ec2-user/app/application-aws.yml \
     -Dspring.profiles.active=real \
     $JAR_NAME 2>&1> $REPOSITORY/nohup.log &
