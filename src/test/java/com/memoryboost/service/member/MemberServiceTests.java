a3dc83a95a6a783eb2d8a1b93f30d1e025459a03
jjh <pursue503@naver.com>
2020-07-10 23:21:51 +0900

    Fix Build Test Error

--- src/test/java/com/memoryboost/service/member/MemberServiceTests.java
+++ src/test/java/com/memoryboost/service/member/MemberServiceTests.java
@@ -1,8 +1,11 @@
 package com.memoryboost.service.member;
 
+import com.memoryboost.domain.dto.member.memoryboost.request.MemberSaveRequestDTO;
 import com.memoryboost.domain.dto.member.memoryboost.request.MemberUpdateRequestDTO;
 import com.memoryboost.domain.dto.member.sns.MemberSNSInfoUpdateRequestDTO;
+import com.memoryboost.domain.entity.member.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,25 @@ public class MemberServiceTests {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Before
+    public void createMember(){
+
+        MemberSaveRequestDTO saveRequestDTO = new MemberSaveRequestDTO();
+        saveRequestDTO.setMemberLoginId("abc1234");
+        saveRequestDTO.setMemberPw("aaa1111");
+        saveRequestDTO.setMemberEmail("abc1234@naver.com");
+        saveRequestDTO.setMemberTel("010-1111-1111");
+        saveRequestDTO.setMemberName("홍길동");
+        saveRequestDTO.setMemberZipCode("00000");
+        saveRequestDTO.setMemberAddress("서울특별시");
+        saveRequestDTO.setMemberDetailAddress("종로구");
+        memberRepository.save(saveRequestDTO.toEntity());
+
+    }
 
 
     @Test
@@ -43,11 +65,13 @@ public class MemberServiceTests {
     @Test
     public void memberUpdate(){
 
+
+
         MemberUpdateRequestDTO updateRequestDTO = new MemberUpdateRequestDTO();
         updateRequestDTO.setMemberPw("aaa1111");
         updateRequestDTO.setMemberTel("010-1111-1111");
 
-        boolean flag = memberService.memberUpdate(32L,updateRequestDTO);
+        boolean flag = memberService.memberUpdate(1L,updateRequestDTO);
         
         if(flag) {
             log.info("업데이트완료");
