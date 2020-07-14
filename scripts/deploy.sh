2484154a4a1e0eac925992a07599859812b09429
jjh <pursue503@naver.com>
2020-07-14 21:57:43 +0900

    Fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+REPOSITORY=/home/ec2-user/app/step2
+
 echo "> deploy.sh에 권한을 부여합니다."
 
 chmod +x ./secondDeploy.sh
 
 echo "> secondDeploy.sh 를 실행합니다."
 
-nohup deploy.sh
+nohup ./secondDeploy.sh \\deploy > $REPOSITORY/deploy.out  2>&1 &
