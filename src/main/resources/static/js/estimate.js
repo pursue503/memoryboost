6ce7584b800f115319d12d66f88c8eae5d59de46
bangchisi <ahdtpf13@naver.com>
2020-07-28 14:52:38 +0900

    #11 For replace script

--- src/main/resources/static/js/estimate.js
+++ src/main/resources/static/js/estimate.js
@@ -210,19 +210,11 @@ $(document).ready(function() {
         var header = $("meta[name='_csrf_header']").attr("content");
 
         if(added.length) {
-            let index = 0;
             for(let obj of added) {
                 let productNo = obj.dataset.productnum;
                 let productCount = $(obj).find("input.count")[0].value;
-                /*let product = new Object();
-                product.productNo = Number(productNo);
-                product.productCount = Number(productCount);
-                orderList.push(product);*/
-                $("#order-form").append("<input type='hidden' name='list["+index+"].productNo' value='"+productNo+"' />")
-                $("#order-form").append("<input type='hidden' name='list["+index+"].productCnt' value='"+productCount+"' />")
-                index++;
             }
-            console.dir(JSON.stringify(orderList));
+
             $.ajax({
                 type : "POST",
                 url : "/order/estimate",
@@ -239,7 +231,7 @@ $(document).ready(function() {
             })
             .fail(function(response) {
                 console.dir("통신 실패");
-            });*/
+            });
         } else {
             alert("견적서가 비어있습니다.");
         }
