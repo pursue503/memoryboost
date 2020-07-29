81c19b9fef1cee51242e0569e906de920912304a
bangchisi <ahdtpf13@naver.com>
2020-07-30 01:10:13 +0900

    [error] #15 image upload fixed

--- src/main/resources/static/js/board/write-summernote.js
+++ src/main/resources/static/js/board/write-summernote.js
@@ -12,49 +12,54 @@ $(document).ready(function() {
             }
           }
     });
-});
 
-//이미지 첨부
-function uploadSummernoteImageFile(file, editor) {
-    var typeFilter = ["jpg", "jpeg", "png", "gif"];
-    var type = file.type.split("/")[1];
-    var typeFlag = 0;
-    for(let filter of typeFilter) {
-        if(type == filter) {
-            typeFlag = 1;
-        }
-    }
-
-    if(typeFlag == 0) {
-        alert("이미지는 [jpg, jpeg, png, gif]만 업로드 가능합니다.");
-        return;
-    }
-
-    if(file.size > 2097152) {
-        alert("이미지 크기는 2MB를 넘을 수 없습니다.");
-        return;
-    }
-
-    var data = new FormData();
-    data.append("file", file);
-
-    var token = $("meta[name='_csrf']").attr("content");
-    var header = $("meta[name='_csrf_header']").attr("content");
-    $.ajax({
-        data : data,
-        type : "POST",
-        url : "/post/image-upload",
-        contentType : false,
-        processData : false,
-        beforeSend : function(xhr) {
-            xhr.setRequestHeader(header, token);
-        },
-        success : function(data) {
-            $(editor).summernote('insertImage', data);
-            if(!isEmpty(data)) {
-                let tag = "<input type='hidden' name='file' value='"+data+"' />"
-                $("#write-form").append(tag);
+    //이미지 첨부
+    function uploadSummernoteImageFile(file, editor) {
+        alert("파일 업로드는 되는것인가 1");
+        var typeFilter = ["jpg", "jpeg", "png", "gif"];
+        var type = file.type.split("/")[1];
+        var typeFlag = 0;
+        for(let filter of typeFilter) {
+            if(type == filter) {
+                typeFlag = 1;
             }
         }
-    });
-}
+
+        if(typeFlag == 0) {
+            alert("이미지는 [jpg, jpeg, png, gif]만 업로드 가능합니다.");
+            return;
+        }
+
+        if(file.size > 2097152) {
+            alert("이미지 크기는 2MB를 넘을 수 없습니다.");
+            return;
+        }
+
+        var data = new FormData();
+        data.append("file", file);
+
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content");
+        $.ajax({
+            data : data,
+            type : "POST",
+            url : "/post/image-upload",
+            contentType : false,
+            processData : false,
+            beforeSend : function(xhr) {
+                alert("파일 업로드는 되는것인가 2");
+                xhr.setRequestHeader(header, token);
+            },
+            success : function(data) {
+                alert("파일 업로드는 되는것인가 3");
+                $(editor).summernote('insertImage', data);
+                if(!isEmpty(data)) {
+                    alert("파일 업로드는 되는것인가 4");
+                    let tag = "<input type='hidden' name='file' value='"+data+"' />"
+                    $("#write-form").append(tag);
+                }
+            }
+        });
+    }
+});
+
